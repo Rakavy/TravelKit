b3e62f183884100e16f78a95ad02bff77dd34a23
al-duss <alex93udssault@gmail.com>
2017-07-31 19:45:37 -0400

    Started React but failed

---
+++ web/js/app.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+			<h1> Title </h1>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(<App/>, document.getElementByID("app"));
